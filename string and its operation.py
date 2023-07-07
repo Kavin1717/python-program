8406c933ac1306cf1629fb095ffdac0741b612a5
Kavin1717 <138850164+Kavin1717@users.noreply.github.com>
2023-07-07 11:49:14 +0530

    Update and rename lab2.py to string and its operation.py

--- string and its operation.py
+++ string and its operation.py
@@ -3,7 +3,7 @@ def can_obtain_string(str1, str2):
     j = 0
 
     while i < len(str1) and j < len(str2):
-        if str1[i] == str2[j]:
+        if str1[i] == str2[j]:https://github.com/Kavin1717/python-program/blob/main/lab2.py
             j += 1
         i += 1
 
